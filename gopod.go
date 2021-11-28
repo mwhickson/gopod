b3d834f38fb2d12784d616832d741683daba267c
Matthew Hickson <matthewhickson@outlook.com>
2021-11-28 15:48:53 -0500

    less crashy in spots where crashy doesn't matter

--- gopod.go
+++ gopod.go
@@ -37,26 +37,25 @@ type Episode struct {
 
 func readSubscriptionsFromOpml(opmlFile string) []Podcast {
 
-	contents, err := os.ReadFile(opmlFile)
-
-	if err != nil {
-		panic(err)
-	}
-
-	var opml OpmlList
-	err = xml.Unmarshal(contents, &opml)
-
-	if err != nil {
-		panic(err)
-	}
-
 	var podcasts []Podcast
 
-	if len(opml.Items) > 0 {
-		podcasts = make([]Podcast, len(opml.Items))
-	
-		for i, item := range opml.Items {
-			podcasts[i] = Podcast {Id: i, Name: item.Text, Url: item.XmlUrl, Episodes: make(map[int]Episode)}
+	contents, err := os.ReadFile(opmlFile)
+
+	if err == nil {
+		var opml OpmlList
+		err = xml.Unmarshal(contents, &opml)
+
+		if err != nil {
+			panic(err)
+		}
+
+
+		if len(opml.Items) > 0 {
+			podcasts = make([]Podcast, len(opml.Items))
+		
+			for i, item := range opml.Items {
+				podcasts[i] = Podcast {Id: i, Name: item.Text, Url: item.XmlUrl, Episodes: make(map[int]Episode)}
+			}
 		}
 	}
 
@@ -66,6 +65,7 @@ func readSubscriptionsFromOpml(opmlFile string) []Podcast {
 
 func displayBanner() {
 
+	// sigh... doesn't work everywhere... (Windows Command Prompt... though Windows Terminal is fine...)
 	const CLEAR string = "\033[2J"
 	const HOME string = "\033[H"
 	fmt.Print(CLEAR, HOME)
@@ -76,7 +76,7 @@ func displayBanner() {
 }
 
 func displayMenu() {
-	fmt.Printf("1) List podcasts \t 2) Play podcast \t 3) Add podcast \t 4) Search for podcasts \t 5) Import OPML \t 6) Export OPML \t 0) Quit\n")
+	fmt.Printf(" 1) Select podcast \n 2) Play episode \n 3) Subscribe to podcast \n 4) Search for podcasts \n 5) Import OPML \n 6) Export OPML \n 7) Settings \n 0) Quit\n")
 	fmt.Println()
 }
 
@@ -97,7 +97,7 @@ func handleMenu() bool {
 	selnum, err = strconv.Atoi(selection)
 
 	if err != nil {
-		panic(err)
+		selnum = -1
 	}
 
 	switch selnum {
@@ -105,13 +105,13 @@ func handleMenu() bool {
 			retval = false
 			
 		case 1:
-			fmt.Println("TODO: listing podcasts...")
+			fmt.Println("TODO: select podcast...")
 			
 		case 2:
-			fmt.Println("TODO: play podcast...")
+			fmt.Println("TODO: play episode...")
 			
 		case 3:
-			fmt.Println("TODO: add podcast...")
+			fmt.Println("TODO: subscribe to podcast...")
 			
 		case 4:
 			fmt.Println("TODO: search for podcasts...")
@@ -121,6 +121,9 @@ func handleMenu() bool {
 			
 		case 6:
 			fmt.Println("TODO: export OPML...")
+
+		case 7:
+			fmt.Println ("TODO: settings...")
 
 		default:
 			// PASS: drop through and re-prompt
@@ -140,7 +143,7 @@ func doImportOpml() {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	s := Subscriptions{Podcasts: readSubscriptionsFromOpml(opmlFile)}
 
 	if (len(s.Podcasts) > 0) {
