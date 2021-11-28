1a9ebb02da907412c4690e22024036d9468cced9
Matthew Hickson <matthewhickson@outlook.com>
2021-11-28 15:18:57 -0500

    clean up code, add menu and handler

--- gopod.go
+++ gopod.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"strconv"
 	"os"
 )
 
@@ -13,7 +14,6 @@ type OpmlList struct {
 
 type OpmlItem struct {
 	Text string `xml:"text,attr"`
-	Title string `xml:"title,attr"`
 	Type string `xml:"type,attr"`
 	XmlUrl string `xml:"xmlUrl,attr"`
 }
@@ -39,14 +39,14 @@ func readSubscriptionsFromOpml(opmlFile string) []Podcast {
 
 	contents, err := os.ReadFile(opmlFile)
 
-	if (err != nil) {
+	if err != nil {
 		panic(err)
 	}
 
 	var opml OpmlList
 	err = xml.Unmarshal(contents, &opml)
 
-	if (err != nil) {
+	if err != nil {
 		panic(err)
 	}
 
@@ -64,11 +64,84 @@ func readSubscriptionsFromOpml(opmlFile string) []Podcast {
 	
 }
 
-func main() {
+func displayBanner() {
 
-	fmt.Println("gopod")
+	const CLEAR string = "\033[2J"
+	const HOME string = "\033[H"
+	fmt.Print(CLEAR, HOME)
+	
+	fmt.Println("gopod - a command line podcast player (in go)")
+	fmt.Println()
+	
+}
 
-	s := Subscriptions{Podcasts: readSubscriptionsFromOpml("sample.opml")}
+func displayMenu() {
+	fmt.Printf("1) List podcasts \t 2) Play podcast \t 3) Add podcast \t 4) Search for podcasts \t 5) Import OPML \t 6) Export OPML \t 0) Quit\n")
+	fmt.Println()
+}
+
+func handleMenu() bool {
+
+	retval := true
+
+	fmt.Print("Enter selection: ")
+
+	var selection string
+	_, err := fmt.Scanln(&selection)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var selnum int
+	selnum, err = strconv.Atoi(selection)
+
+	if err != nil {
+		panic(err)
+	}
+
+	switch selnum {
+		case 0:
+			retval = false
+			
+		case 1:
+			fmt.Println("TODO: listing podcasts...")
+			
+		case 2:
+			fmt.Println("TODO: play podcast...")
+			
+		case 3:
+			fmt.Println("TODO: add podcast...")
+			
+		case 4:
+			fmt.Println("TODO: search for podcasts...")
+			
+		case 5:
+			doImportOpml()
+			
+		case 6:
+			fmt.Println("TODO: export OPML...")
+
+		default:
+			// PASS: drop through and re-prompt
+	}
+
+	return retval
+	
+}
+
+func doImportOpml() {
+
+	fmt.Print("OPML file: ")
+
+	var opmlFile string
+	_, err := fmt.Scanln(&opmlFile)
+
+	if err != nil {
+		panic(err)
+	}
+	
+	s := Subscriptions{Podcasts: readSubscriptionsFromOpml(opmlFile)}
 
 	if (len(s.Podcasts) > 0) {
 		for _, sub := range s.Podcasts {
@@ -76,6 +149,21 @@ func main() {
 		}
 	} else {
 		fmt.Println("No subscriptions found.")
+	}
+
+	fmt.Println()
+
+}
+
+func main() {
+
+	displayBanner()
+
+	isRunning := true
+
+	for isRunning {
+		displayMenu()
+		isRunning = handleMenu()
 	}
 	
 }
